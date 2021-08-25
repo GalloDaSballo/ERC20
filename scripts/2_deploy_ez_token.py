82204abecc8dcaa32fe0c996c44331cf27c0a68e
Geewynn <ekainugodwin@gmail.com>
2021-08-25 09:54:46 +0100

    erc721 file

---
+++ scripts/2_deploy_ez_token.py
@@ -0,0 +1,12 @@
+from brownie import accounts, config, EasyToken
+
+
+
+initial_supply = 1000000000000000000000
+token_name = "TOKEN"
+token_symbol = "TKN"
+
+def main():
+    account = accounts[0]
+    erc20 = EasyToken.deploy(initial_supply, {"from": account}
+        )
